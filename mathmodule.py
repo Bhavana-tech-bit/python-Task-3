75925fe7f18b9e94671d48616b9af3a22bbdf5d9
Bhavana-tech-bit <guduruvimalabhavana@gmail.com>
2025-05-17 22:40:07 +0530

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,8 @@
+import math
+num = float(input("Enter a number:"))
+sqrt_num = math.sqrt(num)
+log_num = math.log(num)
+sin_num = math.sin(num)
+print("Square root:",sqrt_num)
+print("Natural logarithm:",log_num)
+print("Sine(in radians):",sin_num)
